a366446b96fd5fe79929c88765c47b49f4fb4233
Matheus Lazarin <mtslazarin@gmail.com>
2019-05-31 17:40:35 -0300

    Little changes in post_processing.py

--- post_processing.py
+++ post_processing.py
@@ -20,8 +20,10 @@ m.med_to_mat(medName)
 #%% Load all data to workspace
 SM,D = m.load(medName)
 #%% Apply calibration
-calibSig = cp.deepcopy(D.measuredData['calibration']['Orelha E'][0][0])
-calibSig.plot_freq()
-calibdSig = cp.deepcopy(calibSig)
+calibSig = cp.deepcopy(D.measuredData['calibration']['Mic 2'][0][1])
+#calibSig.plot_freq()
+#%%
+calibdSig = cp.deepcopy(D.measuredData['calibration']['Mic 2'][1][2])
 calibdSig.calib_pressure(0,calibSig,1,1000)
-calibdSig.plot_freq()
+#calibdSig.plot_freq()
+print(20*np.log10(max(np.abs(calibdSig.freqSignal))/2**(1/2)/2e-5))
